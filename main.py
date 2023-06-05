6acaafddd91c2797d0c76051238c4aa98009051a
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-05 18:04:18 +0000

    bugfix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/login", methods=['POST', 'GET'])
 def root():
-    if request.method = "GET":
+    if request.method == "GET":
         return render_template("login.html")
     else:
         return request.form

d85a6321eaabcf56919be24b38819065a4bf8cfe
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-05 17:15:24 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_tamplate
+from flask import Flask, render_template, request
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route("/login")
-def login():
-  return render_template("src/login.html")
+def root():
+    return render_template("login.html")
+
+@app.route("/adduser", methods=['POST'])
+def add_user():
+    data = request.fo

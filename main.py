83dd0fb452dee4409e2522b4bfd1b8ce0375a96c
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-13 17:33:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/login", methods=['POST', 'GET'])
-def root():
+def login():
     if request.method == "GET":
         return render_template("login.html")
     else:
         user = request.form["user"]
         return "Hello, " + user
+    
+@app.route("/home")
+def home():
+    return render_template("home.html")

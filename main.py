4c07ce470311b1397600a9d0d665d28312cf8f49
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-15 16:44:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from deta import Deta
+import random, string
 
 app = Flask(__name__)
 deta = Deta()
@@ -15,12 +16,19 @@ def index():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    if request.method == "GET":
+    if request.method == "GET" and not identity.get("u"):
         return render_template("login.html")
+    elif identity.get("u"):
+        return redirect(url_for("index"))
     else:
         user = request.form["user"]
+        id = "".join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20))
+        identity.put({"user": user, "id": id, "tags": []}, "u")
         return "Hello, " + user
     
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    if identity.get("u"):
+        return render_template("home.html")
+    else:
+        return redirect(url_for("index"))

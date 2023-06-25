260aaa6313425136fc1937fea7b51296ce4232a2
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-25 11:49:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from deta import Deta
 import random, string, requests
 import hashlib, os, base64
 
-UPLOAD_FOLDER = '/tmp'
-
 deta = Deta()
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 identity = deta.Base("identity")
-img_drive = deta.Drive("imgs")
+cdn = Base("images")
+images = Drive("images")
 
 @app.route("/")
 def index():
@@ -58,19 +56,34 @@ def upload():
     elif not identity.get("u"):
         return redirect(url_for("index"))
     else:
-        #img = request.files['uploadedIMG']
-        image = request.files['uploadedIMG']
-        # Read the image data
+        image = request.files.get("image")
+        extension = image.filename.split(".")[-1]
         image_data = image.read()
-        # Convert the image data to base64
-        encoded_image = base64.b64encode(image_data).decode('utf-8')
-        #return render_template('img.html', image_data=encoded_image)
-        name = "".join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=10))
-        img_drive.put(name + ".txt", data=encoded_image)
-        response = img_drive.get(img + ".txt")
-        content = response.read()
-        return render_template('img.html', image_data=content)
-        #return name
+        image_data_encoded = base64.b64encode(image_data).decode("utf-8")
+        item = cdn.put(
+            {
+                "ext": extension,
+                "visibility": False,
+                "embed": [{"title": "", "colour": "000000"}],
+            },
+        )
+        id = item["key"]
+        images.put(f"{id}.{extension}", base64.b64decode(image_data_encoded))
+        url = f"{request.scheme}://{request.host}/cdn/{id}.{extension}"
+        return jsonify({"image": url, "id": id})
+     
+           
+@app.route("/cdn/<image>", methods=["GET"])
+def image_cdn(image):
+    img = images.get(image)
+    info = cdn.get(image.split(".")[0])
+    if info["visibility"] == True:
+        return send_file(
+            img.read(),
+            mimetype=f"image/{image.split('.')[1]}",
+        )
+    else:
+        return jsonify({"error": 404})
         
 
 @app.route("/view/<img>")

3f1447ad8d295d4cc5e0ca224eaeed32cf6f63ea
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-06-27 17:30:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import *
 from deta import Deta
-from flask_jsglue import JSGlue
 import random, string, requests
 import hashlib, os, base64
 import io
 
 deta = Deta()
 app = Flask(__name__)
-jsglue = JSGlue(app)
 
 identity = deta.Base("identity")
 img_info = deta.Base("images")

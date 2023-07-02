dca5df2aa15b900cb6164e462334cacf4763a594
oxoovo <102413837+oxoovo@users.noreply.github.com>
2023-07-02 11:22:00 +0200

    Update home.js

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -26,19 +26,12 @@ function heart_click(element) {
 window.onload = function() {
     document.getElementById('content').addEventListener('scroll', addpage);
     function addpage() {
-	/*
         var scrollTop = document.getElementById('content').scrollTop;
         var scrollHeight = document.getElementById('content').scrollHeight;
         var offsetHeight = document.getElementById('content').offsetHeight;
         var contentHeight = scrollHeight - offsetHeight;
-	console.log(contentHeight.toString() + "<=" + scrollTop.toString());
+	document.querySelector(".name").innerHTML = contentHeight.toString() + "<=" + scrollTop.toString();
         if (contentHeight <= scrollTop || firstload) {
-	*/
-	var container = document.getElementById('content');
-	var scrollY = container.scrollHeight - container.scrollTop;
-	var height = container.offsetHeight;
-	var offset = height - scrollY;
-	if (offset == 0 || offset == 1) {
 	    alert("i");
             firstload = false;
 	    const xhttp = new XMLHttpRequest();
